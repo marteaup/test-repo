0916b04bde52aa369ad395163a499084be618f12
Guilherme Almeida <mrgra@icloud.com>
2020-03-12 11:44:07 +0100

    Add express server

---
+++ app/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.json({version:'V1 API'}))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
