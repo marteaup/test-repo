827535dc1777bf2f2c544cda9f038125ceddc261
Guilherme Almeida <mrgra@icloud.com>
2020-03-12 16:34:17 +0100

    Extend the API

--- app/index.js
+++ app/index.js
@@ -2,16 +2,31 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+const users = [
+  {
+    id: 1,
+    username: 'user1',
+    email: 'email1@email.com'
+  },
+  {
+    id: 2,
+    username: 'user2',
+    email: 'email2@email.com'
+  }
+]
 app.get('/', (req, res) => {
   res.json({version:'V1 API'})
 })
 
 app.get('/user', (req,res) => {
-  console.log(req.params)
-  res.json({
-    username: 'MrGRA',
-    email: 'mrgra@icloud.com'
-  })
+  res.json(users)
+})
+
+app.get('/user/:id', (req,res) => {
+  const id = req.params.id
+  const user = users.filter(user => user.id === Number.parseInt(id))[0]
+  res.json(user || {})
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
